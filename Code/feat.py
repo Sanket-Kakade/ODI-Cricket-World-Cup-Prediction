ac6d8b42295110f6daeb87e2b52c1183fd77f81d
Sanket-Kakade <kakadesanket@gmail.com>
2023-11-06 11:17:00 +0530

    code changes

--- Code/feat.py
+++ Code/feat.py
@@ -10,10 +10,13 @@ import pandas as pd
 import numpy as np
 import os
 import glob
+import datetime
 #%%
 all_odi_data_loc= "K:\\Sanket-datascience\\CWC_prediction\\Data\\"
 all_odi_data_file= "odi_results.pkl"
+past_winners_file= "icc_tournaments_winners_list.csv"
 raw_data= pd.read_pickle(all_odi_data_loc+all_odi_data_file)
+past_winners_df= pd.read_csv(all_odi_data_loc+past_winners_file)
 raw_data['team_1_new'] = np.minimum(raw_data['team_1'], raw_data['team_2'])
 raw_data['team_2_new'] = np.maximum(raw_data['team_1'], raw_data['team_2'])
 raw_data['team_1']= raw_data['team_1_new']
@@ -22,48 +25,130 @@ raw_data.drop(['team_1_new','team_2_new'],axis=1,inplace=True)
 # raw_data_latest= raw_data[raw_data['date']>='2015/02/14'].reset_index(drop=True).sort_values('date')
 raw_data_latest= raw_data.copy().sort_values('date')
 teams_ls= sorted(list(pd.unique(raw_data_latest[['team_1', 'team_2']].values.ravel('K'))))
-top_eight_teams_ls= ['South Africa','Australia','New Zealand','England','India','Pakistan','Sri Lanka','West Indies']
+# top_eight_teams_ls= ['South Africa','Australia','New Zealand','England','India','Pakistan','Sri Lanka','West Indies']
+top_eight_teams_ls= ['South Africa','Australia','New Zealand','England','India','Pakistan']
 wc_event_ls= ['ICC Cricket World Cup', 'ICC World Cup']
+#%%
+# No of WC won in past by team
+past_winners_df['date']=pd.to_datetime(past_winners_df['date'])
+winners_pvt= pd.crosstab(past_winners_df['date'], past_winners_df['Winner'].fillna(0))
+winners_pvt=winners_pvt.cumsum()
+winners_pvt = winners_pvt.append(winners_pvt.tail(1)).reset_index()
+winners_pvt.iloc[-1,0] = datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M'), '%Y-%m-%d %H:%M')
+winners_pvt= winners_pvt.set_index('date')
+winners_pvt2= winners_pvt.resample('1D').ffill()
+for i in winners_pvt2.columns:
+    winners_pvt2['test_col']= winners_pvt2[i] - winners_pvt2[i].shift()
+    winners_pvt2[i]= np.where(winners_pvt2['test_col']==1, winners_pvt2[i]-1, winners_pvt2[i])
+winners_pvt2.reset_index(inplace=True)
+winners_pvt2['date'] = winners_pvt2['date'].dt.strftime('%Y/%m/%d')
+winners_pvt2.drop('test_col',axis=1,inplace=True)
+winners_pvt2= winners_pvt2.melt(id_vars=['date'],var_name="team",value_name="no_of_wc_won")
+
+# No of WC runners up in past by team
+past_winners_df['date']=pd.to_datetime(past_winners_df['date'])
+rup_pvt= pd.crosstab(past_winners_df['date'], past_winners_df['Runner-up'].fillna(0))
+rup_pvt=rup_pvt.cumsum()
+rup_pvt = rup_pvt.append(rup_pvt.tail(1)).reset_index()
+rup_pvt.iloc[-1,0] = datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M'), '%Y-%m-%d %H:%M')
+rup_pvt= rup_pvt.set_index('date')
+rup_pvt2= winners_pvt.resample('1D').ffill()
+for i in rup_pvt2.columns:
+    rup_pvt2['test_col']= rup_pvt2[i] - rup_pvt2[i].shift()
+    rup_pvt2[i]= np.where(rup_pvt2['test_col']==1, rup_pvt2[i]-1, rup_pvt2[i])
+rup_pvt2.reset_index(inplace=True)
+rup_pvt2['date'] = rup_pvt2['date'].dt.strftime('%Y/%m/%d')
+rup_pvt2.drop('test_col',axis=1,inplace=True)
+rup_pvt2= rup_pvt2.melt(id_vars=['date'],var_name="team",value_name="no_of_wc_rup")
+
+
+
+# wc_win_data= pd.merge(raw_data_latest[['date']], winners_pvt2, left_on='date',right_on='Date', how='left')
+
+#%%
+# Deriving Batting first team
+conditions = [(raw_data_latest['toss_winner']==raw_data_latest['team_1'])&(raw_data_latest['toss_decision']=='bat'),
+              (raw_data_latest['toss_winner']==raw_data_latest['team_1'])&(raw_data_latest['toss_decision']=='field'),
+              (raw_data_latest['toss_winner']==raw_data_latest['team_2'])&(raw_data_latest['toss_decision']=='bat'),
+              (raw_data_latest['toss_winner']==raw_data_latest['team_2'])&(raw_data_latest['toss_decision']=='field')]
+choices = [raw_data_latest['team_1'], raw_data_latest['team_2'], raw_data_latest['team_2'],raw_data_latest['team_1'] ]
+    
+raw_data_latest["bat_first_team"] = np.select(conditions, choices, default=np.nan)
 #%%
 # team_feats will have the features of each team for eg. team's win ratio, team's last 10 matches win ratio, team's win ratio at home etc. 
 team_feats_df= pd.DataFrame()
 cols_to_drop1= ['team_1','team_2','event', 'venue', 'city', 'toss_winner',
        'toss_decision', 'winner','winner_wickets', 'match_id', 'winner_runs',
        'outcome', 'host_cty', 'mat_flag', 'win_flag', 'win_cum', 'mat_cum','home_mat_flag', 
-       'home_win_flag', 'home_win_cum','mat_vs_top_eight_flag',
-       'win_vs_top_eight_flag', 'win_vs_top_eight_cum', 'mat_vs_top_eight_cum',
-       'home_mat_cum', 'mat_in_wc_flag', 'win_in_wc_flag',
-       'win_in_wc_cum', 'mat_in_wc_cum','win_in_last_ten_mat']
+       'home_win_flag', 'home_win_cum','home_mat_cum',
+       'toss_win_mat_flag','toss_win_mat_win_flag','toss_win_mat_cum','toss_win_mat_win_cum',
+       'bat_first_mat_flag','bat_first_mat_win_flag','bat_first_mat_cum','bat_first_mat_win_cum',
+       'bat_second_mat_flag','bat_second_mat_win_flag','bat_second_mat_cum','bat_second_mat_win_cum',
+       'mat_vs_top_eight_flag','win_vs_top_eight_flag', 'win_vs_top_eight_cum', 'mat_vs_top_eight_cum',
+        'mat_in_wc_flag', 'win_in_wc_flag','win_in_wc_cum', 'mat_in_wc_cum',
+        'win_in_last_ten_mat','win_in_last_five_mat','win_in_last_three_mat','bat_first_team']
 for i in teams_ls:
     tmp_df= raw_data_latest[(raw_data_latest['team_1']==i)|(raw_data_latest['team_2']==i)]
+# Overall win ratio of a team
     tmp_df['mat_flag']= 1
     tmp_df['win_flag']= np.where(tmp_df['winner']==i,1,0)
     tmp_df['win_cum'] = tmp_df['win_flag'].shift().cumsum()
     tmp_df['mat_cum'] = tmp_df['mat_flag'].shift().cumsum()
     tmp_df['win_ratio']= tmp_df['win_cum']/tmp_df['mat_cum']
-    
+
+# Win ratio of a team at their home cty
     tmp_df['home_mat_flag']= np.where(tmp_df['host_cty']==i,1,0)
     tmp_df['home_win_flag']= np.where((tmp_df['host_cty']==i)&(tmp_df['winner']==i),1,0)
     tmp_df['home_win_cum'] = tmp_df['home_win_flag'].shift().cumsum()
     tmp_df['home_mat_cum'] = tmp_df['home_mat_flag'].shift().cumsum()
     tmp_df['win_ratio_home']= tmp_df['home_win_cum']/tmp_df['home_mat_cum']
-    
+
+# Win ratio of a team after winning the toss    
+    tmp_df['toss_win_mat_flag']= np.where(tmp_df['toss_winner']==i,1,0)
+    tmp_df['toss_win_mat_win_flag']= np.where((tmp_df['toss_winner']==i)&(tmp_df['winner']==i),1,0)
+    tmp_df['toss_win_mat_cum'] = tmp_df['toss_win_mat_flag'].shift().cumsum()
+    tmp_df['toss_win_mat_win_cum'] = tmp_df['toss_win_mat_win_flag'].shift().cumsum()
+    tmp_df['win_ratio_toss_win']= tmp_df['toss_win_mat_win_cum']/tmp_df['toss_win_mat_cum']
+
+# Win ratio of a team when batting first
+    tmp_df['bat_first_mat_flag']= np.where(tmp_df['bat_first_team']==i,1,0)
+    tmp_df['bat_first_mat_win_flag']= np.where((tmp_df['bat_first_team']==i)&(tmp_df['winner']==i),1,0)
+    tmp_df['bat_first_mat_cum'] = tmp_df['bat_first_mat_flag'].shift().cumsum()
+    tmp_df['bat_first_mat_win_cum'] = tmp_df['bat_first_mat_win_flag'].shift().cumsum()
+    tmp_df['win_ratio_bat_first']= tmp_df['bat_first_mat_win_cum']/tmp_df['bat_first_mat_cum']
+
+# Win ratio of a team when batting second
+    tmp_df['bat_second_mat_flag']= np.where(tmp_df['bat_first_team']!=i,1,0)
+    tmp_df['bat_second_mat_win_flag']= np.where((tmp_df['bat_first_team']!=i)&(tmp_df['winner']==i),1,0)
+    tmp_df['bat_second_mat_cum'] = tmp_df['bat_second_mat_flag'].shift().cumsum()
+    tmp_df['bat_second_mat_win_cum'] = tmp_df['bat_second_mat_win_flag'].shift().cumsum()
+    tmp_df['win_ratio_bat_second']= tmp_df['bat_second_mat_win_cum']/tmp_df['bat_second_mat_cum']
+
+
+# Win ratio of a team against top teams
     tmp_df['mat_vs_top_eight_flag']= np.where(((tmp_df['team_1']==i)&(tmp_df['team_2'].isin(top_eight_teams_ls)))|((tmp_df['team_1'].isin(top_eight_teams_ls))&(tmp_df['team_2']==i)),1,0)
     tmp_df['win_vs_top_eight_flag']= np.where(((tmp_df['team_1']==i)&(tmp_df['team_2'].isin(top_eight_teams_ls))&(tmp_df['winner']==i))|((tmp_df['team_1'].isin(top_eight_teams_ls))&(tmp_df['team_2']==i)&(tmp_df['winner']==i)),1,0)
     tmp_df['win_vs_top_eight_cum'] = tmp_df['win_vs_top_eight_flag'].shift().cumsum()
     tmp_df['mat_vs_top_eight_cum'] = tmp_df['mat_vs_top_eight_flag'].shift().cumsum()
     tmp_df['win_ratio_vs_top_eight']= tmp_df['win_vs_top_eight_cum']/tmp_df['mat_vs_top_eight_cum']
     
+# Win ratio of a team in world cups
     tmp_df['mat_in_wc_flag']= np.where(tmp_df['event'].isin(wc_event_ls),1,0)
     tmp_df['win_in_wc_flag']= np.where(((tmp_df['winner']==i)&(tmp_df['event'].isin(wc_event_ls))),1,0)
     tmp_df['win_in_wc_cum'] = tmp_df['win_in_wc_flag'].shift().cumsum()
     tmp_df['mat_in_wc_cum'] = tmp_df['mat_in_wc_flag'].shift().cumsum()
     tmp_df['win_ratio_wc']= tmp_df['win_in_wc_cum']/tmp_df['mat_in_wc_cum']
-    
+ 
+# Win ratio of a team in last 5 and 10 matches
     tmp_df['win_in_last_ten_mat'] = tmp_df['win_flag'].rolling(min_periods=1, window=10, closed='left').sum()
     tmp_df['win_ratio_in_last_ten_mat'] = tmp_df['win_in_last_ten_mat']/10
     tmp_df['win_in_last_five_mat'] = tmp_df['win_flag'].rolling(min_periods=1, window=5, closed='left').sum()
-    tmp_df['win_ratio_in_last_five_mat'] = tmp_df['win_in_last_ten_mat']/5
+    tmp_df['win_ratio_in_last_five_mat'] = tmp_df['win_in_last_five_mat']/5
+    tmp_df['win_in_last_three_mat'] = tmp_df['win_flag'].rolling(min_periods=1, window=3, closed='left').sum()
+    tmp_df['win_ratio_in_last_three_mat'] = tmp_df['win_in_last_three_mat']/3
+    tmp_df['win_in_last_mat'] = tmp_df['win_flag'].rolling(min_periods=1, window=1, closed='left').sum()
+    tmp_df['win_in_last_mat'] = tmp_df['win_in_last_mat']/1
+    
     
     tmp_df['team']=i
     tmp_df.drop(cols_to_drop1, axis=1, inplace=True)
@@ -99,7 +184,7 @@ head_to_head_win_loss_pct= pd.DataFrame()
 cols_to_drop2= ['winner','event','venue','city','toss_winner',
        'toss_decision','winner_wickets','match_id','winner_runs',
        'outcome','host_cty','mat_flag','team_1_win_flag','team_2_win_flag',
-       'team_1_win_cum','team_2_win_cum','mat_cum']
+       'team_1_win_cum','team_2_win_cum','mat_cum','bat_first_team','team_1on2_win_last_five_mat_cum','team_2on1_win_last_five_mat_cum']
 for i in teams_ls:
     for j in teams_ls:
 
@@ -114,6 +199,18 @@ for i in teams_ls:
         single_df['mat_cum'] = single_df['mat_flag'].shift().cumsum()
         single_df['win_ratio_team_1on2']= single_df['team_1_win_cum']/single_df['mat_cum']
         single_df['win_ratio_team_2on1']= single_df['team_2_win_cum']/single_df['mat_cum']
+        
+# Win loss ratio in last 5/10 mats
+        single_df['team_1on2_win_last_five_mat_cum'] = single_df['team_1_win_flag'].rolling(min_periods=1, window=5, closed='left').sum()
+        single_df['win_ratio_team_1on2_last_five'] = single_df['team_1on2_win_last_five_mat_cum']/5
+        single_df['team_2on1_win_last_five_mat_cum'] = single_df['team_2_win_flag'].rolling(min_periods=1, window=5, closed='left').sum()
+        single_df['win_ratio_team_2on1_last_five'] = single_df['team_2on1_win_last_five_mat_cum']/5
+        single_df['team_1on2_win_last_ten_mat_cum'] = single_df['team_1_win_flag'].rolling(min_periods=1, window=5, closed='left').sum()
+        single_df['win_ratio_team_1on2_last_ten'] = single_df['team_1on2_win_last_ten_mat_cum']/10
+        single_df['team_2on1_win_last_ten_mat_cum'] = single_df['team_2_win_flag'].rolling(min_periods=1, window=5, closed='left').sum()
+        single_df['win_ratio_team_2on1_last_ten'] = single_df['team_2on1_win_last_ten_mat_cum']/10
+
+        
         
         single_df.drop(cols_to_drop2, axis=1,inplace=True)
         head_to_head_win_loss_pct= head_to_head_win_loss_pct.append(single_df,ignore_index=True)
@@ -135,7 +232,7 @@ head_to_head_win_loss_pct.drop_duplicates(subset=['team_1','team_2','date'], inp
 #%%
 cols_to_drop3= ['team_1','team_2','event','venue','city','toss_winner',
        'toss_decision','winner','winner_wickets','match_id','winner_runs',
-       'outcome','mat_flag','team_win_flag','team_win_cum','mat_cum']
+       'outcome','mat_flag','team_win_flag','team_win_cum','mat_cum','bat_first_team']
 team_win_loss_pct_in_cty= pd.DataFrame()
 for i in teams_ls:
     for j in raw_data_latest['host_cty'].unique():
@@ -161,7 +258,9 @@ for i in teams_ls:
 #         team_host_cty_grid.loc[i,j]= win_ratio_in_cty
 # team_win_loss_pct_in_cty= team_host_cty_grid.unstack().swaplevel().reset_index()
 # team_win_loss_pct_in_cty.columns= ['team','host_cty','team_win_pct_in_cty']
-#### TODO: Check if making the ratio as zero for non playing Cty-Team combination is a right choice. 
+#### TODO: Toss winning impact on winning the match. Win loss ratio.
+#### TODO: bat first, bat second record
+
 #%%
 #Merging and renaming the columns.
 raw_data_latest1= pd.merge(raw_data_latest, team_feats_df, left_on= ['team_1','date'], right_on=['team','date'], how='left')
@@ -170,26 +269,53 @@ raw_data_latest1.rename({'team1_date':'date'},axis=1,inplace=True)
 raw_data_latest2= pd.merge(raw_data_latest1, team_feats_df, left_on= ['team_2','date'], right_on=['team','date'], how='left' )
 raw_data_latest2.columns = ['team2_'+i if i in team_feats_df else i for i in raw_data_latest2.columns]
 raw_data_latest2.rename({'team2_date':'date'},axis=1,inplace=True)
+#%%
+#Code to merge the winners and runners up data with the feature df. 
+raw_data_latest31= pd.merge(raw_data_latest2, winners_pvt2, left_on= ['team_1','date'], right_on=['team','date'], how='left' )
+raw_data_latest31.rename({'no_of_wc_won':'team_1_no_of_wc_won'},axis=1,inplace=True)
+raw_data_latest32= pd.merge(raw_data_latest31, winners_pvt2, left_on= ['team_2','date'], right_on=['team','date'], how='left' )
+raw_data_latest32.rename({'no_of_wc_won':'team_2_no_of_wc_won'},axis=1,inplace=True)
 
+raw_data_latest33= pd.merge(raw_data_latest32, rup_pvt2, left_on= ['team_1','date'], right_on=['team','date'], how='left')
+raw_data_latest33.rename({'no_of_wc_rup':'team_1_no_of_wc_rup'},axis=1,inplace=True)
+raw_data_latest34= pd.merge(raw_data_latest33, rup_pvt2, left_on= ['team_2','date'], right_on=['team','date'], how='left')
+raw_data_latest34.rename({'no_of_wc_rup':'team_2_no_of_wc_rup'},axis=1,inplace=True)
+
+new_cols_created= ['team_1_no_of_wc_won','team_2_no_of_wc_won', 'team_1_no_of_wc_rup', 'team_2_no_of_wc_rup']
+raw_data_latest34[new_cols_created]= raw_data_latest34[new_cols_created].fillna(0)
+raw_data_latest34.drop(['team_x','team_y'],axis=1,inplace=True)
 #%%
 #Removing the tied or no result matches
-raw_data_latest3= raw_data_latest2[(raw_data_latest2['winner']==raw_data_latest2['team_1'])|(raw_data_latest2['winner']==raw_data_latest2['team_2'])]
+raw_data_latest3= raw_data_latest34[(raw_data_latest34['winner']==raw_data_latest34['team_1'])|(raw_data_latest34['winner']==raw_data_latest34['team_2'])]
+#%%
+#Creating binary flags based on the available data and conditions
 raw_data_latest3['winner_encoded']= np.where(raw_data_latest3['winner']==raw_data_latest3['team_1'],'team_1','team_2')
 raw_data_latest3['is_team1_home']= np.where(raw_data_latest3['host_cty']==raw_data_latest3['team_1'],1,0)
 raw_data_latest3['is_team2_home']= np.where(raw_data_latest3['host_cty']==raw_data_latest3['team_2'],1,0)
+raw_data_latest3['cwc_ct_flag']= np.where(raw_data_latest3['event'].isin(wc_event_ls),1,0)
+raw_data_latest3['team_1_top_eight_flag']= np.where(raw_data_latest3['team_1'].isin(top_eight_teams_ls),1,0)
+raw_data_latest3['team_2_top_eight_flag']= np.where(raw_data_latest3['team_2'].isin(top_eight_teams_ls),1,0)
+raw_data_latest3['toss_winner_team_1']= np.where(raw_data_latest3['toss_winner']==raw_data_latest3['team_1'],1,0)
+raw_data_latest3['bat_first_flag_team_1']= np.where(raw_data_latest3['bat_first_team']==raw_data_latest3['team_1'],1,0)
+
 
 #%%
 raw_data_latest4= pd.merge(raw_data_latest3,head_to_head_win_loss_pct, left_on=['team_1', 'team_2','date'], right_on=['team_1','team_2','date']  )
 raw_data_latest5= pd.merge(raw_data_latest4,team_win_loss_pct_in_cty, left_on=['team_1','host_cty','date'], right_on=['team','host_cty','date']  )
 
 #%%
-raw_data_latest5['cwc_ct_flag']= np.where(raw_data_latest5['event'].isin(wc_event_ls),1,0)
-raw_data_latest5['team_1_top_eight_flag']= np.where(raw_data_latest5['team_1'].isin(top_eight_teams_ls),1,0)
-raw_data_latest5['team_2_top_eight_flag']= np.where(raw_data_latest5['team_2'].isin(top_eight_teams_ls),1,0)
+non_nation_cty= ['Africa XI','Asia XI','ICC World XI']
+raw_data_latest6= raw_data_latest5[~(raw_data_latest5['team1_team'].isin(non_nation_cty))&~(raw_data_latest5['team2_team'].isin(non_nation_cty))]
+#%%
+raw_data_latest7 = pd.get_dummies(raw_data_latest6, columns=['team_1','team_2' ])
+
 #%%
 
-cols_to_drop = ['team_1', 'team_2', 'event', 'venue', 'city', 'toss_winner','toss_decision', 'winner', 'winner_wickets', 'match_id', 'winner_runs',
-       'outcome', 'host_cty','team1_team','team2_team','team']
-raw_data_latest6= raw_data_latest5.drop(cols_to_drop,axis=1,inplace=False)
+# cols_to_drop = ['team_1', 'team_2', 'event', 'venue', 'city', 'toss_winner','toss_decision', 'winner', 'winner_wickets', 'match_id', 'winner_runs',
+#        'outcome', 'host_cty','team1_team','team2_team','team']
+cols_to_drop = ['event', 'venue', 'city', 'toss_winner','toss_decision', 'winner', 'winner_wickets', 'match_id', 'winner_runs',
+       'outcome', 'host_cty','bat_first_team','team1_team','team2_team','team']
+
+raw_data_latest8= raw_data_latest7.drop(cols_to_drop,axis=1,inplace=False)
 #%%
-raw_data_latest6.to_pickle('K:\\Sanket-datascience\\CWC_prediction\\Data\\feat_df.pkl')
+raw_data_latest8.to_pickle('K:\\Sanket-datascience\\CWC_prediction\\Data\\feat_df.pkl')
--- Code/modelling.py
+++ Code/modelling.py
@@ -4,40 +4,105 @@ Created on Tue Oct 24 23:37:37 2023
 
 @author: sanket
 """
-
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import f1_score
+from sklearn.svm import SVC
+from sklearn.metrics import f1_score,accuracy_score
 #%%
 all_odi_data_loc= "K:\\Sanket-datascience\\CWC_prediction\\Data\\"
 all_odi_data_file= "feat_df.pkl"
 feat_data= pd.read_pickle(all_odi_data_loc+all_odi_data_file)
-feat_data2= feat_data[feat_data['date']>'2011/1/1']
-feat_data2.drop('date', axis=1,inplace=True)
+feat_data2= feat_data[feat_data['date']>'2015/1/1']
+feat_data2.drop('date',axis=1,inplace=True)
+
 feat_data2.fillna(0,inplace=True)
 #%%
 label_encoder = LabelEncoder()
 feat_data2['winner_le'] = label_encoder.fit_transform(feat_data2['winner_encoded'])
 feat_data2.drop('winner_encoded',axis=1,inplace=True)
-
-x_train,x_test, y_train, y_test= train_test_split(feat_data2.drop('winner_le',axis=1),feat_data2['winner_le'],test_size=0.2)
+dummy_cols= ['team_1_Afghanistan', 'team_1_Australia',
+'team_1_Bangladesh', 'team_1_Bermuda', 'team_1_Canada',
+'team_1_England', 'team_1_Hong Kong', 'team_1_India', 'team_1_Ireland',
+'team_1_Jersey', 'team_1_Kenya', 'team_1_Namibia', 'team_1_Nepal',
+'team_1_Netherlands', 'team_1_New Zealand', 'team_1_Oman',
+'team_1_Pakistan', 'team_1_Papua New Guinea', 'team_1_Scotland',
+'team_1_South Africa', 'team_1_Sri Lanka',
+'team_1_United Arab Emirates', 'team_1_United States of America',
+'team_1_West Indies', 'team_2_Australia',
+'team_2_Bangladesh', 'team_2_Bermuda', 'team_2_Canada',
+'team_2_England', 'team_2_Hong Kong',
+'team_2_India', 'team_2_Ireland', 'team_2_Kenya', 'team_2_Namibia',
+'team_2_Nepal', 'team_2_Netherlands', 'team_2_New Zealand',
+'team_2_Oman', 'team_2_Pakistan', 'team_2_Papua New Guinea',
+'team_2_Scotland', 'team_2_South Africa', 'team_2_Sri Lanka',
+'team_2_United Arab Emirates', 'team_2_United States of America',
+'team_2_West Indies', 'team_2_Zimbabwe']
+feat_data2.drop(dummy_cols, axis=1,inplace=True)
 #%%
-lr= LogisticRegression()
+x_train,x_test, y_train, y_test= train_test_split(feat_data2.drop('winner_le',axis=1),feat_data2['winner_le'],test_size=0.2)
+# train, test= feat_data2[feat_data2['date']<='2020/1/1'], feat_data2[feat_data2['date']>'2020/1/1']
+# train.drop('date',axis=1,inplace=True)
+# test.drop('date',axis=1,inplace=True)
+# x_train,y_train=  train.drop('winner_le',axis=1), train[['winner_le']]
+# x_test, y_test= test.drop('winner_le',axis=1), test[['winner_le']]
+
+#%%
+lr= LogisticRegression(max_iter=10000)
 lr.fit(x_train, y_train)
 y_pred= lr.predict(x_test)
+y_train_pred= lr.predict(x_train)
+print(f1_score(y_train, y_train_pred))
+print(accuracy_score(y_train, y_train_pred))
+
 print(f1_score(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+
 #%%
 rf= RandomForestClassifier()
 rf.fit(x_train, y_train)
 y_pred= rf.predict(x_test)
+y_train_pred= rf.predict(x_train)
+
+print(f1_score(y_train, y_train_pred))
+print(accuracy_score(y_train, y_train_pred))
+
 print(f1_score(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+
 #%%
 gbc= GradientBoostingClassifier()
 gbc.fit(x_train, y_train)
 y_pred= gbc.predict(x_test)
-print(f1_score(y_test, y_pred))
+y_train_pred= gbc.predict(x_train)
 
+print(f1_score(y_train, y_train_pred))
+print(accuracy_score(y_train, y_train_pred))
+print(f1_score(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+#%%
+svc= SVC()
+svc.fit(x_train, y_train)
+y_pred= svc.predict(x_test)
+y_train_pred= svc.predict(x_train)
+
+print(f1_score(y_train, y_train_pred))
+print(accuracy_score(y_train, y_train_pred))
+print(f1_score(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+
+#%%
+# feature_names = [x_train.columns]
+# importances = rf.feature_importances_
+# std = np.std([tree.feature_importances_ for tree in rf.estimators_], axis=0)
+# forest_importances = pd.Series(importances, index=feature_names)
+# forest_importances.sort_values(inplace=True,ascending=False)
+# fig, ax = plt.subplots()
+# forest_importances.plot.bar(yerr=std, ax=ax)
+# ax.set_title("Feature importances using MDI")
+# ax.set_ylabel("Mean decrease in impurity")
+# fig.tight_layout()
